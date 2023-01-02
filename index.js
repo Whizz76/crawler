c76354eaccce28e658b2066758b285785a20f4b9
Aparna <aparna.singh.che20@itbhu.ac.in>
2023-01-03 01:39:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,14 +53,36 @@ const crawler = new PlaywrightCrawler({
 });
 
 log.debug('Adding requests to the queue.');
- if(cluster.isPrimary){
-     // Fork workers.
-        for (let i = 0; i < numCPUs; i++) {
+ if (cluster.isPrimary) {
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', async (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        // IF THERE EXISTS A LINK/LINKS THAT'S STILL NOT PROCESSED THEN FORK A WORKER/CLUSTER
+        await findOne();
+        if (fork) {
             cluster.fork();
         }
-    }
+
+
+    });
+
+}
 app.get('/',async (req,res)=>{
-    res.send("Welcome to Render!");
+   // res.send("Welcome to Render!");
+    if(cluster.isPrimary){
+        res.send(`Master ${process.pid} is running`);
+        await crawler.addRequests([{ url: 'https://www.myntra.com/', label: "MYNTRA" }, { url: 'https://www2.hm.com/en_in/index.html', label: "HNM" }]);
+        await crawler.run();
+    }
+    else{
+        res.send(`Worker ${process.pid} started`);
+        await crawler.addRequests([{ url: "https://www.myntra.com/rain-jacket", label: "MYNTRA CATEGORY|PAGE" }, { url: 'https://www2.hm.com/en_in/women/seasonal-trending/holiday.html', label: "HNM CATEGORY|PAGE" }]);
+        await crawler.run();
+        process.exit(0);
+    }
 });
 app.get('/masterH&M',async (req,res)=>{
     res.send(`Master ${process.pid} is running`);
@@ -91,18 +113,6 @@ app.get('/workerH&M',async (req,res)=>{
 })
 app.get('/workerMyntra',async (req,res)=>{
     res.send(`Worker ${process.pid} started`);
-    if(cluster.isPrimary){
-        cluster.on('exit', async (worker, code, signal) => {
-            console.log(`worker ${worker.process.pid} died`);
-            // IF THERE EXISTS A LINK/LINKS THAT'S STILL NOT PROCESSED THEN FORK A WORKER/CLUSTER
-            await findOne();
-            if (fork) {
-                cluster.fork();
-            }
-    
-    
-        });
-    }
     if(cluster.isWorker){
         await crawler.addRequests([{ url: "https://www.myntra.com/rain-jacket", label: "MYNTRA CATEGORY|PAGE" }]);
         await crawler.run();
