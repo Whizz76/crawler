4edb465f46f8ae7b51665d1ba815277b9d4ab274
Aparna <aparna.singh.che20@itbhu.ac.in>
2023-01-03 15:12:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ import express from "express";
 const app=express();
 var path = process.env.MONGO_URL || url;
 const port=process.env.PORT || 10000;
-app.listen(port,()=>{
-    console.log("server connected");
-})
 // VARIABLE TO CHECK IF THERE IS A NEED TO FORK A WORKER
 var fork = false;
 // CONNECTING TO THE DATABASE THAT CONTAINS ALL THE LINKS THAT NEEDS TO BE DEPLOYED
@@ -35,9 +32,6 @@ async function findOne() {
 }
 // NUMBER OF CORES THAT WE WILL USE
 const numCPUs = (cpus().length) / 2;
-log.setLevel(log.LEVELS.DEBUG);
-
-log.debug('Setting up crawler.');
 const crawler = new PlaywrightCrawler({
     useSessionPool: true,
     persistCookiesPerSession: true,
@@ -51,72 +45,45 @@ const crawler = new PlaywrightCrawler({
     requestHandler: router,
     // headless:false,
 });
-
-log.debug('Adding requests to the queue.');
- if (cluster.isPrimary) {
+if (cluster.isPrimary) {
+    console.log(`Number of CPUs is ${numCPUs}`);
+    console.log(`Master ${process.pid} is running`);
+    log.setLevel(log.LEVELS.DEBUG);
+    log.debug('Setting up crawler.');
+    log.debug('Adding requests to the queue.');
+    await crawler.addRequests([{ url: 'https://www.myntra.com/', label: "MYNTRA" }, { url: 'https://www2.hm.com/en_in/index.html', label: "HNM" }]);
+    await crawler.run();
     // Fork workers.
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
+    // WHENEVER A WORKER DIES THIS WILL GET EXECUTED!
     cluster.on('exit', async (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
         // IF THERE EXISTS A LINK/LINKS THAT'S STILL NOT PROCESSED THEN FORK A WORKER/CLUSTER
-        await findOne();
+        /*await findOne();
         if (fork) {
             cluster.fork();
-        }
-
+        }*/
+        cluster.fork();
 
     });
-
 }
-app.get('/',async (req,res)=>{
-   // res.send("Welcome to Render!");
-    if(cluster.isPrimary){
-        res.send(`Master ${process.pid} is running`);
-        await crawler.addRequests([{ url: 'https://www.myntra.com/', label: "MYNTRA" }, { url: 'https://www2.hm.com/en_in/index.html', label: "HNM" }]);
-        await crawler.run();
-    }
-    else{
-        res.send(`Worker ${process.pid} started`);
+else {
+    app.listen(port, () => {
+        console.log("server connected");
+    })
+    app.get('/', async (req, res) => {
+        res.send("workers started!");
+        console.log(`Worker ${process.pid} is running`);
+        log.setLevel(log.LEVELS.DEBUG);
+        log.debug('Setting up crawler.');
+        log.debug('Adding requests to the queue.');
         await crawler.addRequests([{ url: "https://www.myntra.com/rain-jacket", label: "MYNTRA CATEGORY|PAGE" }, { url: 'https://www2.hm.com/en_in/women/seasonal-trending/holiday.html', label: "HNM CATEGORY|PAGE" }]);
         await crawler.run();
-        process.exit(0);
-    }
-});
-app.get('/masterH&M',async (req,res)=>{
-    res.send(`Master ${process.pid} is running`);
-    if (cluster.isPrimary) {
-        console.log(`Number of CPUs is ${numCPUs}`);
-        console.log(`Master ${process.pid} is running`);
-        await crawler.addRequests([{ url: 'https://www2.hm.com/en_in/index.html', label: "HNM" }]);
-        await crawler.run();
-    }
-});
-app.get('/masterMyntra',async (req,res)=>{
-    res.send(`Master ${process.pid} is running`);
-    if (cluster.isPrimary) {
-        console.log(`Number of CPUs is ${numCPUs}`);
-        console.log(`Master ${process.pid} is running`);
-        await crawler.addRequests([{ url: 'https://www.myntra.com/', label: "MYNTRA" }]);
-        await crawler.run();
-    }
-});
-app.get('/workerH&M',async (req,res)=>{
-    res.send(`Worker ${process.pid} started`);
-    if(cluster.isWorker){
-        await crawler.addRequests([{ url: 'https://www2.hm.com/en_in/women/seasonal-trending/holiday.html', label: "HNM CATEGORY|PAGE" }]);
-        await crawler.run();
         // AFTER CRAWLING IS DONE EXIT THE PROCESS
         process.exit(0);
-    }
-})
-app.get('/workerMyntra',async (req,res)=>{
-    res.send(`Worker ${process.pid} started`);
-    if(cluster.isWorker){
-        await crawler.addRequests([{ url: "https://www.myntra.com/rain-jacket", label: "MYNTRA CATEGORY|PAGE" }]);
-        await crawler.run();
-        // AFTER CRAWLING IS DONE EXIT THE PROCESS
-        process.exit(0);
-    }
-})
+
+    })
+
+}
